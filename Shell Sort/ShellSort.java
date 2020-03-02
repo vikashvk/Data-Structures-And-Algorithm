b7968b7ad93392d35dadb1033d7d8b9a743caf23
Vikash <yequalstovikash@gmail.com>
2020-03-02 08:14:37 +0530

    Shell Sort pull branch

--- Shell Sort/ShellSort.java
+++ Shell Sort/ShellSort.java
@@ -1,5 +1,9 @@
 class ShellSort{
     public static void main(String[] args) {
-        
+        int[] unsortedArray={ 19, 39,-20, 13, 43, 76, 67, 48, -22};
+
+        for(int i=0; i<unsortedArray.length;i++){
+            System.out.println(unsortedArray[i]);
+        }
     }
 }
