8b686eab79de939f28e55d869466560f2b9bf714
Vikash <yequalstovikash@gmail.com>
2020-03-03 07:38:40 +0530

    Knuth Sequence

--- Shell Sort/ShellSort.java
+++ Shell Sort/ShellSort.java
@@ -1,7 +1,7 @@
 class ShellSort{
     public static void main(String[] args) {
         int[] unsortedArray={ 19, 39,-20, 13, 43, 76, 67, 48, -22};
-
+        // Using Knuth Sequence         
         for(int i=0; i<unsortedArray.length;i++){
             System.out.println(unsortedArray[i]);
         }
