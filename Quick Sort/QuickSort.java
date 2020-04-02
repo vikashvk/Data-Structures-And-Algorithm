6210102a9c3baa388583356e9b081c91ad1b83ca
Vikash <yequalstovikash@gmail.com>
2020-04-02 18:09:02 +0530

    Quick Sort

--- Quick Sort/QuickSort.java
+++ Quick Sort/QuickSort.java
@@ -1,5 +1,7 @@
 class QuickSort{
     public static void main(String[] args) {
         int[] unsortedArray={ 19, 39,-20, 13, 43, 76, 67, 48, -22};
+
+        System.out.println(unsortedArray);
     }
 }
