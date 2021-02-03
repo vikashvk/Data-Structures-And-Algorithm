7298f5c0c477cdb0504bc9a9418f5240bf88cb27
Vikash <yequalstovikash@gmail.com>
2021-02-03 22:02:37 +0530

    shell

--- Shell Sort/ShellSort.java
+++ Shell Sort/ShellSort.java
@@ -12,7 +12,7 @@ class ShellSort{
                 }
                 unsortedArray[j]=newElement;                                
             }
-}
+    }
 
         for(int i=0; i<unsortedArray.length;i++){
             System.out.println(unsortedArray[i]);
